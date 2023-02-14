a7dfa14f189e2601f7717cbae63cb0b21f789da7
FULLSTACK <jumin0220@naver.com>
2023-02-14 09:19:47 +0900

    다음영화 셀레니멈

---
+++ crawling04-1.js
@@ -0,0 +1,55 @@
+// 미세먼지 공공데이터를 이용해서 특정 지역의 미세먼지 정보 출력
+// http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty
+// ?serviceKey=???&returnType=json&numOfRows=100&pageNo=1&sidoName=%EC%A0%84%EA%B5%AD&ver=1.0
+
+// 사용할 패키지 가져오기 : require(패키지명)
+const axios = require('axios');
+const { XMLParser } = require('fast-xml-parser');  // xml 처리기 라이브러리
+
+async function main() {   // 비동기 I/O 지원 함수 정의
+
+    // 접속할 url, 쿼리스트링, user-agent 헤더 지정
+    // 인증 vs 인가
+    const URL = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty';
+    const params = {'serviceKey': 'O1nHnnvHdTlHJn9IB55OEVZsW8tvqkALFXNRBo9nGXjODXETiIA9SjoOZAyLAE3OyADFq+mhF36yldlCJppS2A==',
+        'returnType': 'xml', 'sidoName': '서울', 'numOfRows':1000, 'ver':1.3
+    };
+    const headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78'};
+
+    // axios로 접속해서 대기오염 정보를 받아옴
+    const xml = await axios.get(URL,{
+        params : params, headers : headers
+    });   // 서버 요청시 User-Agent 헤더 사용
+
+    // 받아온 데이터 잠시 확인
+    // console.log(xml.data);
+
+    // XML을 JSON으로 변환하기
+    const parser = new XMLParser();
+    let json = parser.parse(xml.data);
+
+    // JSON 으로 불러오기
+    let items = json['response']['body']['items'];
+    // console.log(items);
+
+    // 미세먼지 정보 출력
+    for (let item of items['item']) {
+        console.log(item.sidoName, item.stationName,
+            item.pm10Value, item.pm25Value,
+            item.pm10Grade, item.pm25Grade,
+            pmGrade(item.pm10Grade), pmGrade(item.pm25Grade),
+            item.dataTime);
+    }
+
+};
+
+// 등급별 이모지
+// 😍 😐 😰 😱
+
+let pmGrade = (val) => {
+    let emojis = ['😍','😐','😰','😱'];
+
+    return emojis[parseInt(val) - 1];
+};
+
+main();
--- crwaling06.js
+++ crwaling06.js
@@ -1,88 +1,79 @@
-//영화 제목추출
-//https://movie.daum.net/main
-//https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty
-//?serviceKey=jpe6L3k%2B4bb1WZhf5gztMjF8ceaW2SgEwh1V%2BglIC7ZxX8O6z8UjBdE6f3pnrkeHz1%2Bnp8lGb0drUp5bqZq%2Fqw%3D%3D&returnType=xml&numOfRows=100&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&ver=1.0
+// movie.daum.net 사이트에서 '상영중인 영화'에 대한 정보를 긁어오기
+// https://movie.daum.net/main
+// 순위, 영화제목, 예약율, 평점
 
-//사용할 패키지 가져오기:require()
-const axios=require('axios'); //
-const cheerio=require('cheerio'); //dom 라이브러리
-const {Builder,Browser,By,Key,until}=require('selenium-webdriver')//알트앤터 눌러 생성하기
+// 사용할 패키지 가져오기 : require(패키지명)
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
+const url = require("url");
 
-async function main () {
+async function main() {   // 비동기 I/O 지원 함수 정의
 
-    //접속할 url지정
-    const URl = 'https://movie.daum.net/main';
+    // 접속할 url 지정
+    const URL = 'https://movie.daum.net/main';
 
-    //크롬자동화 즈라우져 객체 생성
-    const chrome= await new Builder().forBrowser(Browser.CHROME)
+    // 크롬 자동화 브라우져 객체 생성
+    const chrome = await new Builder().forBrowser(Browser.CHROME)
         .setChromeOptions()
         .build();
 
-    try{
-        //지정한  url로 접속
-        await chrome.get(URl);
+    try {
+        // 지정한 url로 접속
+        await chrome.get(URL);
 
-        //특정 요소가 화면에 위치할대까지 최대 5초간 기다려줌
-        await chrome.wait(until.elementLocated(By.css('.feature_home div:nth-child(3).slide_ranking .tit_item')),5000);
+        // 특정 요소가 화면에 위치할때까지 최대 5초간 기다려 줌
+        await chrome.wait(
+            until.elementLocated(By.css(
+                '.feature_home div:nth-child(3).slide_ranking .tit_item')), 5000);
 
-        //접속한 사이트의 html 소스가져옴
-        const html= await chrome.getPageSource();
-       // console.log(html);
+        // 접속한 사이트의 html 소스를 가져옴
+        const html = await chrome.getPageSource();
+        // console.log(html);
 
-        //5초 정도 잠시 대기
-       // await chrome.sleep(5000);//여기는 안써도 되니 참고만 하고
-        //페이지 소스를 dom객체로 변환
+        // 페이지소스를 dom객체로 변환
         const dom = cheerio.load(html);
 
-        //영화제목추출,평점 에매율추출
-        let movies=dom('.feature_home div:nth-child(3).slide_ranking .tit_item');
-        let rates=dom('.feature_home div:nth-child(3).slide_ranking .tit_num:first-child');
-        let rsrvs =dom('.feature_home div:nth-child(3).slide_ranking .tit_num:last-child');
+        // 영화제목들, 평점, 예매율 추출
+        let movies = dom('.feature_home div:nth-child(3).slide_ranking .tit_item');
+        let rates = dom('.feature_home div:nth-child(3).slide_ranking .txt_num:first-child');
+        let rsrvs = dom('.feature_home div:nth-child(3).slide_ranking .txt_num:last-child');
 
-        //추출한 결과를 저장하기 위한 배열 선언
-        let moviess=[],ratess=[],rsrvss=[];
+        // 추출한 결과를 저장하기 위한 배열 선언
+        let moviess = [], ratess = [], rsrvss = [];
 
-        //추출된 영화 ,제목 출력
-        movies.each((idx,movie)=>{
-            let title=dom(movie).text().trim()
-         // console.log(title);
+        // 추출된 영화제목 출력
+        movies.each((idx, movie) => {
+            let title = dom(movie).text().trim();
+            // console.log(title);
             moviess.push(title);
-
         });
 
-        //평점추출
-
-        rates.each((idx,rate)=>{
-            let point =dom(rate).text().trim();
-            //console.log(point);
-            ratess.push(point);
+        // 추출된 평점 출력
+        rates.each((idx, rate) => {
+            let point = dom(rate).text().trim();
+            // console.log(point);
+            ratess.push(point)
         });
 
-
-
-        //예매율추출
-
-        rsrvs.each((idx,rsrv)=>{
-            let rsrt= dom(rsrv).text().trim();
-           // console.log(rsrt);
+        // 추출된 예매율 출력
+        rsrvs.each((idx, rsrv) => {
+            let rsrt = dom(rsrv).text().trim();
+            // console.log(rsrt);
             rsrvss.push(rsrt);
         });
 
-
-        //한변에 모아서 출력
-        for (let i=0;i <moviess.length; ++i) {
+        // 한번에 모아서 출력
+        for(let i = 0; i < moviess.length; ++i) {
             console.log(`${moviess[i]} ${ratess[i]} ${rsrvss[i]}`);
         }
 
-
-
-    }catch (ex){
+    } catch (ex) {
         console.log(ex);
-
-    }finally {
-        await chrome.quit();//크롬브라우져 닫기 작업이 끝나면 닫아주기
+    } finally {
+        await chrome.quit();   // 크롬 브라우져 닫기
     }
 
-
 };
+
 main();
